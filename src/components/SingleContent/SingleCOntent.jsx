2afa033d64b92a169d433400b23464d1e04088d0
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-21 14:42:47 +0530

    added badeg

--- src/components/SingleContent/SingleCOntent.jsx
+++ src/components/SingleContent/SingleCOntent.jsx
@@ -1,10 +1,17 @@
+import { Badge } from "@mui/material";
 import React from "react";
 import { img_300, unavailable } from "../../config/config";
 import "./SingleContent.css";
 
 const SingleCOntent = ({ id, poster, title, date, mediaType, voteAvg }) => {
+  const vote = voteAvg.toFixed(1);
   return (
     <div className="media">
+      <Badge
+        badgeContent={vote}
+        color={voteAvg > 6 ? "primary" : "secondary"}
+      />
+
       <img
         className="poster"
         src={poster ? `${img_300}/${poster}` : unavailable}
