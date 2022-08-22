267c3faccf97bfe4e1e8fe26faabafab814551c7
“Reemu04” <reemu.glsica17@gmail.com>
2022-08-22 10:13:59 +0530

    search component done

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,11 +1,38 @@
-import { Button, Tab, Tabs, TextField } from "@mui/material";
-import React, { useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
+import { Tab, Tabs, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SingleCOntent from "../../components/SingleContent/SingleCOntent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
 const Search = () => {
   const [type, setType] = useState(0);
   const [page, setPage] = useState(1);
-  const setSearchText = () => {};
+  const [searchText, setSearchText] = useState("");
+  const [content, setContent] = useState([]);
+  const [noOfPages, setNoOfPages] = useState();
 
+  const fetchSearch = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+        process.env.REACT_APP_API_KEY
+      }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+    );
+    console.log(data);
+    setContent(data.results);
+    setNoOfPages(data.total_pages);
+  };
+
+  const onChangeSearch = (e) => {
+    setSearchText(e.target.value);
+    console.log("hi", e.target.value.trim().length);
+    if (e.target.value.trim().length === 0) {
+      fetchSearch();
+    }
+  };
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSearch();
+    // eslint-disable-next-line
+  }, [type, page, searchText]);
   return (
     <>
       <div style={{ display: "flex", margin: "15px 0" }}>
@@ -14,19 +41,21 @@ const Search = () => {
           className="searchbox"
           label="Search"
           variant="filled"
-          onChange={(e) => setSearchText(e.target.value)}
+          onChange={(e) => onChangeSearch(e)}
         />
-        <Button
+        {/* <Button
           sx={{
             background: "white",
           }}
           variant="contained"
           style={{ marginLeft: 10 }}
+          onClick={fetchSearch}
         >
           <SearchIcon />{" "}
-        </Button>
+        </Button> */}
       </div>
       <Tabs
+        sx={{ display: "flex", justifyContent: "center" }}
         value={type}
         indicatorColor="primary"
         textColor="primary"
@@ -35,9 +64,42 @@ const Search = () => {
           setPage(1);
         }}
       >
-        <Tab style={{ width: "50%" }} label="Search Movies" />
-        <Tab style={{ width: "50%" }} label="Search TV Series" />
+        <Tab style={{ width: "50rem" }} label="Search Movies" />
+        <Tab style={{ width: "50rem" }} label="Search TV Series" />
       </Tabs>
+      <div className="trending">
+        {console.log("hi", content)}
+        {searchText.length > 0 &&
+          content &&
+          content.map((c) => (
+            <SingleCOntent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              mediaType={type ? "tv" : "movie"}
+              voteAvg={c.vote_average}
+            />
+          ))}
+        {!content.length > 0 && type === 1 ? (
+          <h2>No Series Found</h2>
+        ) : (
+          "" ||
+          (searchText.length === 0 &&
+            (type === 1 ? <h2>No Series Found</h2> : ""))
+        )}
+        {!content.length > 0 && type === 0 ? (
+          <h2>No Movies Found</h2>
+        ) : (
+          "" ||
+          (searchText.length === 0 &&
+            (type === 0 ? <h2>No Movies Found</h2> : ""))
+        )}
+      </div>
+      {searchText.length > 0 && noOfPages > 1 && (
+        <CustomPagination setPage={setPage} noOfPages={noOfPages} />
+      )}
     </>
   );
 };
